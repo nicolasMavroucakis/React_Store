9903b6105aaa3df38cac3b7f950f91bdbb22b241
Nicolas de Almeida MAvroucakis <nicolas.mavroucakis@gmail.com>
2024-03-27 16:32:35 -0300

    a

--- React_Store/React_Store/src/components/crud/index.jsx
+++ React_Store/React_Store/src/components/crud/index.jsx
@@ -3,12 +3,11 @@ import StartFirebase from "../FirebaseConfig";
 import { ref, set, update, remove, get, child } from 'firebase/database';
 import { Link } from "react-router-dom";
 
-
 export class Crud extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            db: '',
+            db: null,
             username: '',
             fullname: '',
             phonenumber: '',
@@ -24,6 +23,121 @@ export class Crud extends React.Component {
             db: StartFirebase()
         });
     }
+
+    interface(event) {
+        const id = event.target.id;
+
+        if (id === 'addBtn') {
+            this.insertData();
+        }
+        else if (id === 'updateBtn') {
+            this.updateData();
+        }
+        else if (id === 'deleteBtn') {
+            this.deleteData();
+        }
+        else if (id === 'selectBtn') {
+            this.selectData();
+        }
+        else if (id === 'logBtn') {
+            this.verifyData();
+        }
+    }
+
+    getAllInputs() {
+        return {
+            username: this.state.username,
+            name: this.state.fullname,
+            phone: this.state.phonenumber,
+            dob: this.state.dob,
+            password: this.state.password
+        }
+    }
+
+    insertData() {
+        const { db } = this.state;
+        const data = this.getAllInputs();
+
+        set(ref(db, 'Customer/' + data.username), {
+            Fullname: data.name,
+            Phonenumber: data.phone,
+            dateofbirth: data.dob,
+            password: data.password
+        })
+        .then(() => { alert('Data was added successfully'); })
+        .catch((error) => { alert('There was a problem adding the data: ' + error.message); });
+    }
+
+    updateData() {
+        const { db } = this.state;
+        const data = this.getAllInputs();
+
+        update(ref(db, 'Customer/' + data.username), {
+            Fullname: data.name,
+            Phonenumber: data.phone,
+            dateofbirth: data.dob,
+            password: data.password
+        })
+        .then(() => { alert('Data was updated successfully'); })
+        .catch((error) => { alert('There was a problem updating the data: ' + error.message); });
+    }
+
+    deleteData() {
+        const { db } = this.state;
+        const data = this.getAllInputs();
+
+        remove(ref(db, 'Customer/' + data.username))
+        .then(() => { alert('Data was deleted successfully'); })
+        .catch((error) => { alert('There was a problem deleting the data: ' + error.message); });
+    }
+
+    selectData() {
+        const { db } = this.state;
+        const data = this.getAllInputs();
+
+        get(child(ref(db), 'Customer/' + data.username))
+        .then((snapshot) => {
+            if (snapshot.exists()) {
+                this.setState({
+                    fullname: snapshot.val().Fullname,
+                    phonenumber: snapshot.val().Phonenumber,
+                    dob: snapshot.val().dateofbirth,
+                    password: snapshot.val().password,
+                });
+            } else {
+                alert('No data found for this username');
+            }
+        })
+        .catch((error) => { alert('There was a problem fetching the data: ' + error.message); });
+    }
+
+    verifyData() {
+        const { db } = this.state;
+        const data = this.getAllInputs();
+
+        get(child(ref(db), 'Customer/' + data.username))
+            .then((snapshot) => {
+                let fullname_ver = '';
+                let password_ver = '';
+                if (snapshot.exists()) {
+                    fullname_ver = snapshot.val().Fullname;
+                    password_ver = snapshot.val().password;
+                } else {
+                    alert('No data found for this username');
+                }
+                if (data.username === fullname_ver && data.password === password_ver) {
+                    this.setState({ dadosVerificado: true }, () => {
+                        if (this.state.dadosVerificado) {
+                            this.props.history.push("/");
+                        } else {
+                            alert("dados incompatíveis");
+                        }
+                    });
+                }
+            })
+            .catch((error) => { alert('There was a problem fetching the data: ' + error.message); });
+    }
+
     render() {
         const pathname =  window.location.pathname
         let user_is_verified = false
@@ -43,7 +157,7 @@ export class Crud extends React.Component {
                 </>
             )
         }
-        else if ( pathname == '/signin') {
+        else if (pathname === '/signin') {
             return (
                 <>
                     <label>Enter Username</label>
@@ -98,119 +212,4 @@ export class Crud extends React.Component {
             )
         }
     }
-
-    interface(event) {
-        const id = event.target.id;
-
-        if (id === 'addBtn') {
-            this.insertData();
-        }
-        else if (id === 'updateBtn') {
-            this.updateData();
-        }
-        else if (id === 'deleteBtn') {
-            this.deleteData();
-        }
-        else if (id === 'selectBtn') {
-            this.selectData();
-        }
-        else if (id === 'logBtn') {
-            this.verifyData();
-        }
-    }
-
-    getAllInputs() {
-        return {
-            username: this.state.username,
-            name: this.state.fullname,
-            phone: this.state.phonenumber,
-            dob: this.state.dob,
-            password: this.state.password
-        }
-    }
-
-    insertData() {
-        const db = this.state.db;
-        const data = this.getAllInputs();
-
-        set(ref(db, 'Customer/' + data.username), {
-            Fullname: data.name,
-            Phonenumber: data.phone,
-            dateofbirth: data.dob,
-            password: data.password
-        })
-        .then(() => { alert('Data was added successfully'); })
-        .catch((error) => { alert('There was a problem adding the data: ' + error.message); });
-    }
-
-    updateData() {
-        const db = this.state.db;
-        const data = this.getAllInputs();
-
-        update(ref(db, 'Customer/' + data.username), {
-            Fullname: data.name,
-            Phonenumber: data.phone,
-            dateofbirth: data.dob,
-            password: data.password
-        })
-        .then(() => { alert('Data was updated successfully'); })
-        .catch((error) => { alert('There was a problem updating the data: ' + error.message); });
-    }
-
-    deleteData() {
-        const db = this.state.db;
-        const data = this.getAllInputs();
-
-        remove(ref(db, 'Customer/' + data.username))
-        .then(() => { alert('Data was deleted successfully'); })
-        .catch((error) => { alert('There was a problem deleting the data: ' + error.message); });
-    }
-
-    selectData() {
-        const db = this.state.db;
-        const data = this.getAllInputs();
-
-        get(child(ref(db), 'Customer/' + data.username))
-        .then((snapshot) => {
-            if (snapshot.exists()) {
-                this.setState({
-                    fullname: snapshot.val().Fullname,
-                    phonenumber: snapshot.val().Phonenumber,
-                    dob: snapshot.val().dateofbirth,
-                    password: snapshot.val().password,
-                });
-            } else {
-                alert('No data found for this username');
-            }
-        })
-        .catch((error) => { alert('There was a problem fetching the data: ' + error.message); });
-    }
-
-    verifyData() {
-        const db = this.state.db;
-        const data = this.getAllInputs();
-
-        get(child(ref(db), 'Customer/' + data.username))
-            .then((snapshot) => {
-                let fullname_ver = '';
-                let password_ver = '';
-                if (snapshot.exists()) {
-                    fullname_ver = snapshot.val().Fullname;
-                    password_ver = snapshot.val().Password;
-                } else {
-                    alert('No data found for this username');
-                }
-                if (data.username === fullname_ver && data.password === password_ver) {
-                    this.setState({ dadosVerificado: true }, () => {
-                        if (this.state.dadosVerificado) {
-                            this.props.history.push("/");
-                        } else {
-                            alert("dados incompatíveis");
-                        }
-                    });
-                }
-            })
-            .catch((error) => { alert('There was a problem fetching the data: ' + error.message); });
-    }
-
 }
